d774472c8600e88e5d1bd61e9227573b981fda69
Lucas Alessio <lucas.alessio@info.ufrn.br>
2020-12-16 08:48:45 -0300

    Uso dos dados do banco no gráfico de consumo

--- src/main/java/com/ufrn/embarcados/reaqua/controller/DashboardController.java
+++ src/main/java/com/ufrn/embarcados/reaqua/controller/DashboardController.java
@@ -1,12 +1,9 @@
 package com.ufrn.embarcados.reaqua.controller;
 
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,7 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.ufrn.embarcados.reaqua.model.Tower;
+import com.ufrn.embarcados.reaqua.model.WaterTank;
+import com.ufrn.embarcados.reaqua.model.WaterTankData;
 import com.ufrn.embarcados.reaqua.service.TowerService;
+import com.ufrn.embarcados.reaqua.service.WaterTankDataService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,45 +25,90 @@ public class DashboardController {
 
 	private final TowerService towerService;
 
+	private final WaterTankDataService waterTankDataService;
+	
+	/** Number of days to query the consumption records */
+	private final Integer lookbackPeriod = 6;
+
 	@RequestMapping(value = "/dashboard", method = RequestMethod.GET)
 	public String dashboard(Model model){
 		List<Tower> towers = towerService.listAll();
 		//TODO substituir pela torre do usuário logado 
 		Tower selectedTower = towerService.findById(1L).get();
-		//TODO substituir por dados consultados do banco
-		Map<String, Integer> waterConsumption = getWaterConsumptionMap();
-		
+
 		model.addAttribute("selectedTower", selectedTower);
 		model.addAttribute("towers", towers);
-		model.addAttribute("dates", waterConsumption.keySet());
-		model.addAttribute("consumption", Arrays.asList(waterConsumption.values()));
+		model.addAttribute("dates", getWaterConsumptionDateList());
+		model.addAttribute("consumption", getWaterConsumption(selectedTower));
 		return "dashboard";
 	}
-	
+
 	@RequestMapping(value = "/dashboard/{id}", method = RequestMethod.GET)
 	public String towerBoard(Model model, @PathVariable String id){
 		List<Tower> towers = towerService.listAll();
 		Tower selectedTower = towerService.findById(Long.valueOf(id)).get();
-		//TODO substituir por dados consultados do banco
-		Map<String, Integer> waterConsumption = getWaterConsumptionMap();
-		
+
 		model.addAttribute("selectedTower", selectedTower);
 		model.addAttribute("towers", towers);
-		model.addAttribute("dates", waterConsumption.keySet());
-		model.addAttribute("consumption", Arrays.asList(waterConsumption.values()));
+		model.addAttribute("dates", getWaterConsumptionDateList());
+		model.addAttribute("consumption", getWaterConsumption(selectedTower));
 		return "dashboard";
 	}
-	
-	private Map<String, Integer> getWaterConsumptionMap() {
-		Map<String, Integer> map = new LinkedHashMap<String, Integer>();
-		Calendar date = Calendar.getInstance();
-		date.add(Calendar.DATE, -6);
+
+	private List<String> getWaterConsumptionDateList() {
+		List<String> dates = new ArrayList<String>();
 		SimpleDateFormat format = new SimpleDateFormat("EEE");
-		for (int i=0; i<7; i++) {
-			map.put("\'" + format.format(date.getTime()) + "\'", (int) (Math.random()*100));
+		Calendar date = Calendar.getInstance();
+		date.add(Calendar.DATE, -lookbackPeriod);
+
+		for (int i=0; i<=lookbackPeriod; i++) {
+			dates.add("\'" + format.format(date.getTime()) + "\'");
 			date.add(Calendar.DATE, 1);
 		}
-		return map;
+
+		return dates;
+	}
+
+	private List<List<Integer>> getWaterConsumption(Tower tower) {
+		List<List<Integer>> lists = new ArrayList<List<Integer>>();
+
+		for (WaterTank tank: tower.getWaterTanks()) {
+			List<Integer> consumption = new ArrayList<Integer>();
+			Calendar startDate = Calendar.getInstance();
+			Calendar endDate = Calendar.getInstance();
+			startDate.add(Calendar.DATE, -lookbackPeriod);
+			startDate.set(startDate.get(Calendar.YEAR), startDate.get(Calendar.MONTH), startDate.get(Calendar.DATE), 0, 0, 0);
+			endDate.add(Calendar.DATE, -lookbackPeriod);
+			endDate.set(endDate.get(Calendar.YEAR), endDate.get(Calendar.MONTH), endDate.get(Calendar.DATE), 23, 59, 59);
+
+			for (int i=0; i<=lookbackPeriod; i++) {
+				List<WaterTankData> data = waterTankDataService.findByWaterTankAndDate(tank, startDate.getTime(), endDate.getTime());
+
+				Integer waterUse = 0;
+				Integer waterFill = 0;
+				Integer previousCapacity = null;
+				for (WaterTankData d: data) {
+					if (previousCapacity != null) {
+						if (d.getCapacity() > previousCapacity) {
+							waterFill += d.getCapacity();
+						} else {
+							waterUse += d.getCapacity();
+						}
+					}
+
+					previousCapacity = d.getCapacity();
+					System.out.println(d.toString());
+				}
+
+				startDate.add(Calendar.DATE, 1);
+				endDate.add(Calendar.DATE, 1);
+				consumption.add(waterUse);
+			}
+
+			lists.add(consumption);
+		}
+
+		return lists;
 	}
 
 }
--- src/main/java/com/ufrn/embarcados/reaqua/controller/WaterTankController.java
+++ src/main/java/com/ufrn/embarcados/reaqua/controller/WaterTankController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.ufrn.embarcados.reaqua.model.Tower;
+import com.ufrn.embarcados.reaqua.model.WaterTankData;
 import com.ufrn.embarcados.reaqua.service.TowerService;
+import com.ufrn.embarcados.reaqua.service.WaterTankDataService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,11 +21,20 @@ public class WaterTankController {
 	
 	private final TowerService towerService;
 	
+	private final WaterTankDataService waterTankDataService;
+	
 	@RequestMapping(value = "/waterTanks", method = RequestMethod.GET)
 	public String waterTanksBoard(Model model){
 		List<Tower> towers = towerService.listAll();
 		model.addAttribute("towers", towers);
 		return "waterTanks";
+	}
+	
+	@RequestMapping(value = "/waterTanksData", method = RequestMethod.GET)
+	public String waterTanksDataBoard(Model model){
+		List<WaterTankData> data = waterTankDataService.listAll();
+		model.addAttribute("data", data);
+		return "waterTanksData";
 	}
 
 }
